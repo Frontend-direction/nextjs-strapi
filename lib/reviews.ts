893e82a24fda1e6e8a9dbdb533294e8270d3d1f1
Volodymyr Pankiv <vpanki@softserveinc.com>
2024-05-14 09:33:45 +0300

    added cms connections
    Signed-off-by: Volodymyr Pankiv <vpanki@softserveinc.com>

--- lib/reviews.ts
+++ lib/reviews.ts
@@ -1,12 +1,22 @@
-import { readdir, readFile } from "node:fs/promises";
-import matter from "gray-matter";
+import { readdir } from "node:fs/promises";
 import { marked } from "marked";
+import qs from "qs";
+
+const CMS_URL = "http://localhost:1337";
+
+interface CmsItem {
+  id: number;
+  attributes: any;
+}
 
 export interface Review {
   slug: string;
   title: string;
   date: string;
   image: string;
+}
+
+export interface FullReview extends Review {
   body: string | Promise<string>;
 }
 
@@ -16,29 +26,63 @@ export async function getFeaturedReview(): Promise<Review> {
   return firstReview;
 }
 
-export async function getReview(slug: string): Promise<Review> {
-  const text = await readFile(`./content/reviews/${slug}.md`, "utf8");
-  const {
-    content,
-    data: { title, date, image },
-  } = matter(text);
-  const body = marked(content);
-  return { title, date, image, body, slug };
+export async function getReview(slug: string): Promise<FullReview> {
+  const { data } = await fetchReviews({
+    filters: { slug: { $eq: slug } },
+    fields: ["slug", "title", "subtitle", "publishedAt", "body"],
+    populate: { image: { fields: ["url"] } },
+    pagination: { pageSize: 1, withCount: false },
+  });
+  const { attributes } = data[0];
+  return {
+    ...toReview(data[0]),
+    body: marked(attributes.body),
+  };
 }
 
 export async function getReviews(): Promise<Review[]> {
-  const slugs = await getSlugs();
-  const reviews: Review[] = [];
-  for (const slug of slugs) {
-    const review = await getReview(slug);
-    reviews.push(review);
+  const { data } = await fetchReviews({
+    fields: ["slug", "title", "subtitle", "publishedAt"],
+    populate: { image: { fields: ["url"] } },
+    sort: ["publishedAt:desc"],
+    pagination: { pageSize: 6 },
+  });
+
+  return data.map(toReview);
+}
+
+async function fetchReviews(parameters: any) {
+  const url =
+    `${CMS_URL}/api/reviews?` +
+    qs.stringify(parameters, { encodeValuesOnly: true });
+  console.log("[fetchReviews]:", url);
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`CMS returned ${response.status} for ${url}`);
   }
-  return reviews.sort((a, b) => a.date.localeCompare(b.date));
+  return await response.json();
 }
 
 export async function getSlugs(): Promise<string[]> {
-  const files = await readdir("./content/reviews");
-  return files
-    .filter((file) => file.endsWith(".md"))
-    .map((file) => file.slice(0, -".md".length));
+  // const files = await readdir("./content/reviews");
+  // return files
+  //   .filter((file) => file.endsWith(".md"))
+  //   .map((file) => file.slice(0, -".md".length));
+  const { data } = await fetchReviews({
+    fields: ["slug"],
+    sort: ["publishedAt:desc"],
+    pagination: { pageSize: 100 },
+  });
+  return data.map((item: CmsItem) => item.attributes.slug);
+}
+
+function toReview(item: CmsItem): Review {
+  const { attributes } = item;
+
+  return {
+    slug: attributes.slug,
+    title: attributes.title,
+    date: attributes.publishedAt.slice(0, "yyyy-mm-dd".length),
+    image: CMS_URL + attributes.image.data.attributes.url,
+  };
 }
